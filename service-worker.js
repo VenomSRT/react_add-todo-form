9e7205bff73a4804df794b3a6ba242e058a2da32
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-09-09 23:18:22 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.ecfd8ac09dbb43e6dcfac4c3238d8dfd.js"
 );
 
 self.addEventListener('message', (event) => {
